cde4514600432a4fc989a7c1222b675e7e99e131
haiyaapain <lolibanzai484@gmail.com>
2025-09-27 19:21:17 +0700

    Update HomeController.java

--- src/main/java/com/clinic/controller/HomeController.java
+++ src/main/java/com/clinic/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "index"; // looks for src/main/resources/templates/index.html
+        return "index"; // load templates/index.html
     }
 }
