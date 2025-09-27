0d1b03576a58486fd90b2896cff9b6a039ce2caf
haiyaapain <lolibanzai484@gmail.com>
2025-09-27 19:49:49 +0700

    Update PatientRepository.java

--- src/main/java/com/clinic/repository/PatientRepository.java
+++ src/main/java/com/clinic/repository/PatientRepository.java
@@ -4,6 +4,14 @@ import com.clinic.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+
+    Optional<Patient> findByEmail(String email);
+
+
+    Optional<Patient> findByPhoneNumber(String phoneNumber);
 }
