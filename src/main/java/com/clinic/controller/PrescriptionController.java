f826b658f84fe5bc4e4a081709938f86428c22be
haiyaapain <lolibanzai484@gmail.com>
2025-09-27 19:48:02 +0700

    Update PrescriptionController.java

--- src/main/java/com/clinic/controller/PrescriptionController.java
+++ src/main/java/com/clinic/controller/PrescriptionController.java
@@ -3,8 +3,10 @@ package com.clinic.controller;
 import com.clinic.model.Prescription;
 import com.clinic.service.PrescriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -14,13 +16,26 @@ public class PrescriptionController {
     @Autowired
     private PrescriptionService prescriptionService;
 
-    @GetMapping
-    public List<Prescription> getAllPrescriptions() {
-        return prescriptionService.getAllPrescriptions();
+    @PostMapping("/{token}")
+    public ResponseEntity<Prescription> addPrescription(
+            @PathVariable String token,
+            @Valid @RequestBody Prescription prescription) {
+        Prescription savedPrescription = prescriptionService.addPrescription(token, prescription);
+        return ResponseEntity.ok(savedPrescription);
     }
 
-    @PostMapping
-    public Prescription addPrescription(@RequestBody Prescription prescription) {
-        return prescriptionService.addPrescription(prescription);
+    @GetMapping
+    public ResponseEntity<List<Prescription>> getAllPrescriptions() {
+        return ResponseEntity.ok(prescriptionService.getAllPrescriptions());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Prescription> getPrescriptionById(@PathVariable Long id) {
+        Prescription prescription = prescriptionService.getPrescriptionById(id);
+        if (prescription != null) {
+            return ResponseEntity.ok(prescription);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
