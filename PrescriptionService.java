fc04cfc4c87692ec20826108c9bbdeec5bde6caf
haiyaapain <lolibanzai484@gmail.com>
2025-09-27 10:20:44 +0700

    Create PrescriptionService.java

---
+++ PrescriptionService.java
@@ -0,0 +1,23 @@
+package com.clinic.service;
+
+import com.clinic.model.Prescription;
+import com.clinic.repository.PrescriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PrescriptionService {
+
+    @Autowired
+    private PrescriptionRepository prescriptionRepository;
+
+    public List<Prescription> getAllPrescriptions() {
+        return prescriptionRepository.findAll();
+    }
+
+    public Prescription addPrescription(Prescription prescription) {
+        return prescriptionRepository.save(prescription);
+    }
+}
