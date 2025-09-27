9be1a04c2e67d5c38cf926468574ccdbf0e180e8
haiyaapain <lolibanzai484@gmail.com>
2025-09-27 10:16:50 +0700

    Update DoctorController.java

--- DoctorController.java
+++ DoctorController.java
@@ -1,11 +1,10 @@
-package com.smartclinic.controller;
+package com.clinic.controller;
 
-import com.smartclinic.model.Doctor;
-import com.smartclinic.service.DoctorService;
+import com.clinic.model.Doctor;
+import com.clinic.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -15,40 +14,13 @@ public class DoctorController {
     @Autowired
     private DoctorService doctorService;
 
-    // Get all doctors
     @GetMapping
     public List<Doctor> getAllDoctors() {
         return doctorService.getAllDoctors();
     }
 
-    // Get doctors available at a specific time
-    @GetMapping("/available")
-    public List<Doctor> getAvailableDoctors(@RequestParam("time") String time) {
-        LocalDateTime dateTime = LocalDateTime.parse(time);
-        return doctorService.getAvailableDoctors(dateTime);
-    }
-
-    // Get doctor by ID
-    @GetMapping("/{id}")
-    public Doctor getDoctorById(@PathVariable Long id) {
-        return doctorService.getDoctorById(id);
-    }
-
-    // Add new doctor
     @PostMapping
     public Doctor addDoctor(@RequestBody Doctor doctor) {
         return doctorService.addDoctor(doctor);
-    }
-
-    // Update doctor details
-    @PutMapping("/{id}")
-    public Doctor updateDoctor(@PathVariable Long id, @RequestBody Doctor doctor) {
-        return doctorService.updateDoctor(id, doctor);
-    }
-
-    // Delete doctor
-    @DeleteMapping("/{id}")
-    public void deleteDoctor(@PathVariable Long id) {
-        doctorService.deleteDoctor(id);
     }
 }
