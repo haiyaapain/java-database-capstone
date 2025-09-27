8f5997efaa2a7767dd6ecfcd30d44aa7aff318b2
haiyaapain <lolibanzai484@gmail.com>
2025-09-27 19:47:02 +0700

    Update AppointmentRepository.java

--- src/main/java/com/clinic/repository/AppointmentRepository.java
+++ src/main/java/com/clinic/repository/AppointmentRepository.java
@@ -3,7 +3,13 @@ package com.clinic.repository;
 import com.clinic.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDoctorIdAndDate(Long doctorId, LocalDate date);
 }
